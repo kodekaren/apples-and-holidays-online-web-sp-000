591c0891d9e32af6dc3ba3b7ace57197dd94ba99
karen ercoli <karenercoli@gmail.com>
2018-11-27 19:11:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -66,4 +66,4 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-end
+
