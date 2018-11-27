eca382b271d443b2ff87c2175d5ed095314ad93b
karen ercoli <karenercoli@gmail.com>
2018-11-27 19:11:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,10 +60,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 end
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
 end
-
